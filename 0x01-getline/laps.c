9fa67c1ab259b3737f4fb28be8dd0502a6025563
Samuel Pomeroy <1690@holbertonschool.com>
2020-12-09 06:33:49 +0000

    0x01 first working build; not sure if helpers allowed

---
+++ 0x01-getline/laps.c
@@ -0,0 +1,119 @@
+#include "laps.h"
+
+/**
+ * car_list_add_node - adds new node to linked list of racecar ids
+ * @cars: list of car profiles
+ * @id: new id to profile
+ * Return: new node in list
+ */
+racecar_t *car_list_add_node(racecar_t *cars, int id)
+{
+	racecar_t *new = NULL;
+
+	new = malloc(sizeof(racecar_t));
+	if (!new)
+	{
+		fprintf(stderr, "race_state: Can't malloc!\n");
+		free_car_list(cars);
+	}
+	new->id = id;
+	new->laps = 0;
+	new->new_entry = true;
+
+	return (new);
+}
+
+/**
+ * print_car_list - prints linked list of racecar ids and lap counts
+ * @cars: list of car profiles
+ */
+void print_car_list(racecar_t *cars)
+{
+	racecar_t *temp = NULL;
+
+	temp = cars;
+	while (temp)
+	{
+		if (temp->new_entry)
+			printf("Car %i joined the race\n", temp->id);
+		temp = temp->next;
+	}
+
+	printf("Race state:\n");
+	temp = cars;
+	while (temp)
+	{
+		printf("Car %i [%i laps]\n", temp->id, temp->laps);
+		temp->new_entry = false;
+		temp = temp->next;
+	}
+}
+
+/**
+ * free_car_list - frees linked list of racecar ids and lap counts
+ * @cars: list of car profiles
+ */
+void free_car_list(racecar_t *cars)
+{
+	racecar_t *head = NULL, *temp = NULL;
+
+	head = cars;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
+/**
+ * race_state - Tracks lap postion of race cars, updating with every successive
+ * call. Every value in either adds a lap to that car's total or adds a new car
+ * to the list of competitors
+ * @id: array of car ids
+ * @size: amount of ids in array `id`
+ */
+void race_state(int *id, size_t size)
+{
+	static racecar_t *cars;
+	racecar_t *new = NULL, *temp = NULL;
+	size_t i;
+
+	if (size == 0)
+	{
+		free_car_list(cars);
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		temp = cars;
+		/* find insertion point */
+		while (temp && temp->next && temp->next->id <= id[i])
+			temp = temp->next;
+		if (temp && temp->id == id[i])
+		{
+			temp->laps++;
+			continue;
+		}
+
+		new = car_list_add_node(cars, id[i]);
+		if (!new)
+			return;
+
+		/* insert at head */
+		if (!temp || (temp == cars && temp->id > id[i]))
+		{
+			new->next = temp;
+			cars = (racecar_t *)new;
+		}
+		else
+		{
+			new->next = temp->next;
+			temp->next = new;
+		}
+	}
+
+	print_car_list(cars);
+}
---
+++ 0x01-getline/laps.h
@@ -0,0 +1,28 @@
+#ifndef LAPS_H
+#define LAPS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * struct racecar_s - singly linked list node
+ * @id: dirver id#
+ * @laps: completed lap count
+ * @new_entry: was this entry added on this function call?
+ * @next: pointer to next node
+ */
+typedef struct racecar_s
+{
+	int id;
+	int laps;
+	bool new_entry;
+	struct racecar_s *next;
+} racecar_t;
+
+racecar_t *car_list_add_node(racecar_t *cars, int id);
+void print_car_list(racecar_t *cars);
+void free_car_list(racecar_t *cars);
+void race_state(int *id, size_t size);
+
+#endif /* LAPS_H */
