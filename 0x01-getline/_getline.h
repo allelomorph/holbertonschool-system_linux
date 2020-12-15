7743ebc7088653505502552f27ee5b32a227940a
Samuel Pomeroy <1690@holbertonschool.com>
2020-12-15 00:11:48 +0000

    first working build for all tasks

--- 0x01-getline/_getline.h
+++ 0x01-getline/_getline.h
@@ -14,22 +14,36 @@
 #include <stdio.h>
 #include <errno.h>
 
-/*
+/* macros */
+#define READ_SIZE 1024
+
+/* structs */
+/**
+ * struct fd_profile_s - node in a singly linked list of fd profiles
+ *
+ * @fd: file descriptor to read
+ * @read_buf: buffer of READ_SIZE bytes to read into
+ * @line_start: starting index of next \n delimited line
+ * @next: pointer to next node in list
+ */
 typedef struct fd_profile_s
 {
 	int fd;
 	char *read_buf;
 	size_t line_start;
-	fd_profile_s *next;
+	struct fd_profile_s *next;
 } fd_profile_t;
-*/
 
-#define READ_SIZE 1024
+/* functions */
+/* (up to 5 helpers allowed to _getline() */
 
+void free_profiles(fd_profile_t **head);
+fd_profile_t *add_profile(fd_profile_t  **head, int fd);
+char *join_line(char **unfinished_line, char **new_line,
+		size_t unfinished_line_len, size_t line_len);
 char *copy_line(char *read_buf, size_t *line_start, bool *overflow);
 char *buff_load(int fd, char *read_buf, size_t line_start);
-char *_getline(const int fd);
 
-/* total of 5 helpers to _getline allowed */
+char *_getline(const int fd);
 
 #endif /* _GETLINE_H */
