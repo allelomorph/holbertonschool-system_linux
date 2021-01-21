a39a6258168e1e7e607f3008d660c192a4eaf169
Samuel Pomeroy <1690@holbertonschool.com>
2021-01-21 19:19:03 +0000

    first attempt t0; missing tests/

--- 0x03-proc_filesystem/example_heap.c
+++ 0x03-proc_filesystem/example_heap.c
@@ -4,25 +4,43 @@
 #include <stdlib.h>
 /* strcpy strlen */
 #include <string.h>
-/* sleep */
+/* sleep getpid */
 #include <unistd.h>
+/* getpid (pid_t) */
+#include <sys/types.h>
 /* sigaction */
 #include <signal.h>
+/* bool */
 #include <stdbool.h>
 
 
-static bool breakLoop = false;
+static bool breakLoop;
 
+/**
+ * interruptHandler - sets boolean to break out of infinite loop in main
+ *
+ * @signo: signal number; only SIGINT handled by this function
+ */
 static void interruptHandler(int signo)
 {
 	if (signo == SIGINT)
 		breakLoop = true;
 }
 
+
+/**
+ * main - entry point; takes first arg and allocates space on the heap for
+ * that string, then prints string and address until SIGINT
+ *
+ * @argc: amount of command line args
+ * @argv: array of command line args
+ * Return: EXIT_SUCCESS (0) on success, EXIT_FAILURE (1) on failure
+ */
 int main(int argc, char **argv)
 {
 	char *str;
 	int i;
+	pid_t pid;
 	struct sigaction act;
 
 	memset(&act, 0, sizeof(act));
@@ -33,7 +51,7 @@ int main(int argc, char **argv)
 	{
 		fprintf(stderr, "%s usage: %s <string to repeat>\n",
 			argv[0], argv[0]);
-		return (1);
+		return (EXIT_FAILURE);
 	}
 	else
 	{
@@ -41,19 +59,20 @@ int main(int argc, char **argv)
 		if (!str)
 		{
 			fprintf(stderr, "%s: malloc failure\n", argv[0]);
-			return (1);
+			return (EXIT_FAILURE);
 		}
 		strcpy(str, argv[1]);
 	}
 
+	pid = getpid();
 	/* intentionally infinite loop to monitor alterations in heap */
 	for (i = 1; !breakLoop; i++)
 	{
-		printf("[%2d] \"%s\" @ %p\n", i, str, (void *)str);
+		printf("[%i]\t\"%s\" @ %p\tpid: %i\n", i, str, (void *)str, pid);
 		sleep(1);
 	}
 
 	free(str);
 
-	return (0);
+	return (EXIT_SUCCESS);
 }
---
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+"""
+0x03. Python - /proc filesystem - task 0. Hack the VM
+
+    Searches the heap of a process for a given string and replaces it with
+another.
+"""
+from sys import argv, exit
+
+
+def read_write_heap(pid, read_str, write_str):
+    """
+    Search the heap of process correponding to `pid` for `read_str` and replace
+    it with `write_str`.
+
+    Args:
+        pid (int): process id of currently ongoing process' heap to search
+        read_str (str): string to search for in heap
+        write_str (str): string to replace `read_str` with
+    """
+
+    bad_pid = False
+    try:
+        pid_n = int(pid)
+    except (TypeError, ValueError):
+        bad_pid = True
+    if pid_n < 0:
+        bad_pid = True
+
+    if bad_pid:
+        print("Missing or invalid pid!")
+        print("Usage: read_write_heap(pid, read_str, write_str)")
+        exit(1)
+
+    if type(read_str) != str or read_str == "":
+        print("Missing or invalid read string!")
+        print("Usage: read_write_heap(pid, read_str, write_str)")
+        exit(1)
+
+    if type(write_str) != str or write_str == "":
+        print("Missing or invalid write string!")
+        print("Usage: read_write_heap(pid, read_str, write_str)")
+        exit(1)
+
+    try:
+        maps = open("/proc/{}/maps".format(pid), 'r')
+    except OSError as error:
+        print("Can't open file /proc/{}/maps: OSError: {}".format(pid, error))
+        exit(1)
+    print("[*] maps: /proc/{}/maps".format(pid))
+    print("[*] mem: /proc/{}/mem".format(pid))
+
+    heap_data = None
+    for line in maps:
+        if "heap" in line:
+            heap_data = line.split()
+    maps.close()
+
+    if heap_data is None:
+        print("No heap found!")
+        exit(1)
+    else:
+        print('\n'.join(("[*] Found: {}:".format(heap_data[-1]),
+                         "\tpathname = {}".format(heap_data[-1]),
+                         "\taddress range = {}".format(heap_data[0]),
+                         "\tpermissions = {}".format(heap_data[1]),
+                         "\toffset (in bytes) = {}".format(heap_data[2]),
+                         "\tinode = {}".format(heap_data[4]))))
+
+    addr = heap_data[0].split('-')
+    print("[*] Addresses start [{}] | end [{}]".format(addr[0].lstrip('0'),
+                                                       addr[1].lstrip('0')))
+
+    perms = heap_data[1]
+    if 'r' not in perms:
+        print("Heap does not have read permission")
+        exit(0)
+    if 'w' not in perms:
+        print("Heap does not have write permission")
+        exit(0)
+
+    try:
+        mem = open("/proc/{}/mem".format(pid), 'rb+')
+    except OSError as error:
+        print("Can't open file /proc/{}/maps: OSError: {}".format(pid, error))
+        exit(1)
+
+    heap_start = int(addr[0], 16)
+    heap_end = int(addr[1], 16)
+    mem.seek(heap_start)
+    heap = mem.read(heap_end - heap_start)
+    str_offset = heap.find(bytes(read_str, "ASCII"))
+    if str_offset == -1:
+        print("Can't find {} in /proc/{}/mem".format(read_str, pid))
+        exit(1)
+    else:
+        print("[*] Found '{}' at {}".format(read_str, hex(str_offset)[2:]))
+
+    mem.seek(heap_start + str_offset)
+    mem.write(bytes(write_str + '\0', "ASCII"))
+    print("[*] Writing '{}' at {}".format(write_str,
+                                          hex(heap_start + str_offset)[2:]))
+
+
+if __name__ == "__main__":
+
+    if (len(argv) != 4):
+        print("Usage: {} <pid> <string to read> "
+              "<string to write>".format(argv[0]))
+        exit(1)
+    else:
+        pid = argv[1]
+        target_str = argv[2]
+        new_str = argv[3]
+        read_write_heap(pid, target_str, new_str)
