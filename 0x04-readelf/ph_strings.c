ecd02c227f4d973ec6a3d64c67e4d31630ef3de5
Samuel Pomeroy <1690@holbertonschool.com>
2021-02-01 07:03:48 +0000

    0x04 debugged t2

--- 0x04-readelf/ph_strings.c
+++ 0x04-readelf/ph_strings.c
@@ -28,53 +28,12 @@ const char *getSegType(Elf64_Word p_type)
 	case PT_GNU_EH_FRAME: return "GNU_EH_FRAME";
 	case PT_GNU_STACK:return "GNU_STACK";
 	case PT_GNU_RELRO:  return "GNU_RELRO";
-/* not in elf.h
-	case PT_GNU_PROPERTY: return "GNU_PROPERTY";
-	case PT_OPENBSD_RANDOMIZE: return "OPENBSD_RANDOMIZE";
-	case PT_OPENBSD_WXNEEDED: return "OPENBSD_WXNEEDED";
-	case PT_OPENBSD_BOOTDATA: return "OPENBSD_BOOTDATA";
-*/
 	default:
-/*
-		if ((p_type >= PT_LOPROC) && (p_type <= PT_HIPROC))
-		{
-		* use helper functions to find special seg types for the following machine codes:
-		EM_AARCH64 EM_ARM EM_MIPS EM_MIPS_RS3_LE EM_PARISC EM_IA_64 EM_TI_C6000 EM_S390 EM_S390_OLD *
-*/
-/*
+		/* ignoring PT_LOPROC to PT_HIPROC and related EM_ codes */
 		if ((p_type >= PT_LOOS) && (p_type <= PT_HIOS))
-		{
-			const char * result = NULL;
-
-			switch (filedata->file_header.e_ident[EI_OSABI])
-			{
-			case ELFOSABI_GNU:
-			case ELFOSABI_FREEBSD:
-				if (p_type >= PT_GNU_MBIND_LO && p_type <= PT_GNU_MBIND_HI)
-				{
-					sprintf (buff, "GNU_MBIND+%#lx", p_type - PT_GNU_MBIND_LO);
-					result = buff;
-				}
-				break;
-			case ELFOSABI_HPUX:
-				result = get_hpux_segment_type (p_type,
-								filedata->file_header.e_machine);
-				break;
-			case ELFOSABI_SOLARIS:
-				result = get_solaris_segment_type (p_type);
-				break;
-			default:
-				break;
-			}
-
-			if (result != NULL)
-				return result;
-
-			sprintf (buff, "LOOS+%#lx", p_type - PT_LOOS);
-		}
+			sprintf (buff, "LOOS+%x", p_type - PT_LOOS);
 		else
-*/
-		snprintf (buff, sizeof(buff), "<unknown>: %x", p_type);
+			snprintf (buff, sizeof(buff), "<unknown>: %x", p_type);
 	}
 
 	return buff;
@@ -126,99 +85,3 @@ int getProgInterp(re_state *state)
 	}
 	printf("\ni at loop exit: %lu\n", i);
 */
-
-#ifdef ZZZ
-switch (p_type)
-{
-case PT_NULL:return "NULL";
-case PT_LOAD:return "LOAD";
-case PT_DYNAMIC:return "DYNAMIC";
-case PT_INTERP:return "INTERP";
-case PT_NOTE:return "NOTE";
-case PT_SHLIB:return "SHLIB";
-case PT_PHDR:return "PHDR";
-case PT_TLS:return "TLS";
-case PT_GNU_EH_FRAME: return "GNU_EH_FRAME";
-case PT_GNU_STACK:return "GNU_STACK";
-case PT_GNU_RELRO:  return "GNU_RELRO";
-case PT_GNU_PROPERTY: return "GNU_PROPERTY";
-
-case PT_OPENBSD_RANDOMIZE: return "OPENBSD_RANDOMIZE";
-case PT_OPENBSD_WXNEEDED: return "OPENBSD_WXNEEDED";
-case PT_OPENBSD_BOOTDATA: return "OPENBSD_BOOTDATA";
-
-default:
-	if ((p_type >= PT_LOPROC) && (p_type <= PT_HIPROC))
-	{
-		const char * result;
-
-		switch (filedata->file_header.e_machine)
-		{
-		case EM_AARCH64:
-			result = get_aarch64_segment_type (p_type);
-			break;
-		case EM_ARM:
-			result = get_arm_segment_type (p_type);
-			break;
-		case EM_MIPS:
-		case EM_MIPS_RS3_LE:
-			result = get_mips_segment_type (p_type);
-			break;
-		case EM_PARISC:
-			result = get_parisc_segment_type (p_type);
-			break;
-		case EM_IA_64:
-			result = get_ia64_segment_type (p_type);
-			break;
-		case EM_TI_C6000:
-			result = get_tic6x_segment_type (p_type);
-			break;
-		case EM_S390:
-		case EM_S390_OLD:
-			result = get_s390_segment_type (p_type);
-			break;
-		default:
-			result = NULL;
-			break;
-		}
-
-		if (result != NULL)
-			return result;
-
-		sprintf (buff, "LOPROC+%#lx", p_type - PT_LOPROC);
-	}
-	else if ((p_type >= PT_LOOS) && (p_type <= PT_HIOS))
-	{
-		const char * result = NULL;
-
-		switch (filedata->file_header.e_ident[EI_OSABI])
-		{
-		case ELFOSABI_GNU:
-		case ELFOSABI_FREEBSD:
-			if (p_type >= PT_GNU_MBIND_LO && p_type <= PT_GNU_MBIND_HI)
-			{
-				sprintf (buff, "GNU_MBIND+%#lx", p_type - PT_GNU_MBIND_LO);
-				result = buff;
-			}
-			break;
-		case ELFOSABI_HPUX:
-			result = get_hpux_segment_type (p_type,
-							filedata->file_header.e_machine);
-			break;
-		case ELFOSABI_SOLARIS:
-			result = get_solaris_segment_type (p_type);
-			break;
-		default:
-			break;
-		}
-		if (result != NULL)
-			return result;
-
-		sprintf (buff, "LOOS+%#lx", p_type - PT_LOOS);
-	}
-	else
-		snprintf (buff, sizeof (buff), _("<unknown>: %lx"), p_type);
-
-	return buff;
-}
-#endif
