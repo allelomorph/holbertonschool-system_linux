c7109384f6c868cb1f7eeee0062aa03e515b0dea
Samuel Pomeroy <1690@holbertonschool.com>
2021-05-12 19:36:02 +0000

    0x0A t0,1 first attempts

--- 0x0A-multithreading/0-thread_entry.c
+++ 0x0A-multithreading/0-thread_entry.c
@@ -1,11 +1,23 @@
 #include "multithreading.h"
+/* pthread_exit */
+#include <pthread.h>
+/* puts */
+#include <stdio.h>
+
 
 /**
  * thread_entry - entry point to a new thread
  *
  * @arg: address of a string that must be printed followed by '\n'
- * Return: ?? TBD
+ * Return: NULL by default, void pointer could be used for modified struct
+ *   coming in via `arg`
  */
 void *thread_entry(void *arg)
 {
+	if (!arg)
+		pthread_exit(NULL);
+
+	puts((char *)arg);
+
+	pthread_exit(NULL);
 }
--- 0x0A-multithreading/1-tprintf.c
+++ 0x0A-multithreading/1-tprintf.c
@@ -1,11 +1,35 @@
 #include "multithreading.h"
+/* pthread_t pthread_self */
+#include <pthread.h>
+/* va_list */
+#include <stdarg.h>
+/* printf vprintf */
+#include <stdio.h>
+
 
 /**
  * tprintf - uses the printf family to print out a given formatted string
  *
  * @format: format string for printed output, followed by vargs
- * Return: ?? TBD (number of bytes printed, as returned by printf-alikes?)
+ * Return: number of bytes printed or 0 on failure
  */
 int tprintf(char const *format, ...)
 {
+	va_list valist;
+	int id_bytes = 0, msg_bytes = 0;
+
+	/* man 3 pthread_self: "This function always succeeds." */
+	id_bytes = printf("[%lu] ", pthread_self());
+
+	va_start(valist, format);
+	msg_bytes = vprintf(format, valist);
+	va_end(valist);
+
+	if (msg_bytes == -1)
+	{
+		putchar('\n');
+		msg_bytes = 0;
+	}
+
+	return (id_bytes + msg_bytes);
 }
