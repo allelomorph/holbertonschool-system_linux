a2964ce88fc3ff45680c169c26bc6218920e1786
Samuel Pomeroy <1690@holbertonschool.com>
2021-06-30 09:07:03 +0000

    0x0C specialized server output t4-7

--- 0x0C-sockets/6-printRequest.c
+++ 0x0C-sockets/6-printRequest.c
@@ -5,16 +5,20 @@
 
 
 /**
- * printRequest - prints Request-Line fields of a vaildated HTTP request
+ * printRequest - prints headers of a vaildated HTTP request
  *
  * @request: pointer to struct populated with processed request contents
  */
 void printRequest(HTTP_request_t *request)
 {
+	HTTP_header_t *hdr;
+
 	if (!request)
 		return;
 
-	printf("Method: %s\nPath: %s\nVersion: %s\n",
-	       request->Method, request->Request_URI,
-	       request->HTTP_Version);
+	for (hdr = request->headers; hdr; hdr = hdr->next)
+	{
+		printf("Header: \"%s\" -> \"%s\"\n",
+		       hdr->name, hdr->value);
+	}
 }
---
+++ 0x0C-sockets/7-printRequest.c
@@ -0,0 +1,45 @@
+#include "API_server.h"
+
+/* printf */
+#include <stdio.h>
+/* strtok */
+#include <string.h>
+
+/**
+ * printRequest - interprets and prints the URL-encoded query in the body of
+ *   a vaildated HTTP request
+ *
+ * @request: pointer to struct populated with processed request contents
+ */
+void printRequest(HTTP_request_t *request)
+{
+	char *kv_pair1, *kv_pair2;
+	char *key1, *key2, *value1, *value2;
+
+	if (!request || !request->Request_URI || !request->message_body)
+		/* response 500? */
+		return;
+
+	kv_pair1 = strtok(request->message_body, "&");
+	kv_pair2 = strtok(NULL, "&");
+	if (!kv_pair1 || !kv_pair2 || strtok(NULL, "&"))
+		/* response 400? */
+		return;
+
+	key1 = strtok(kv_pair1, "=");
+	value1 = strtok(NULL, "=");
+	if (!key1 || !value1 || strtok(NULL, "="))
+		/* response 400? */
+		return;
+
+	key2 = strtok(kv_pair2, "=");
+	value2 = strtok(NULL, "=");
+	if (!key2 || !value2 || strtok(NULL, "="))
+		/* response 400? */
+		return;
+
+	printf("Path: %s\n", request->Request_URI);
+	printf("Body param: \"%s\" -> \"%s\"\n", key1, value1);
+	printf("Body param: \"%s\" -> \"%s\"\n", key2, value2);
+	/* response 200? */
+}
