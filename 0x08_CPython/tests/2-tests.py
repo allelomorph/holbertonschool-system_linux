43df46a195c3d1814834b339943538347a78b57d
Samuel Pomeroy <1690@holbertonschool.com>
2021-12-09 22:20:21 +0000

    0x08 removed semicolons from provided 2-tests.py, 3-tests.py

--- 0x08_CPython/tests/2-tests.py
+++ 0x08_CPython/tests/2-tests.py
@@ -4,11 +4,11 @@ lib = ctypes.CDLL('./libPython.so')
 lib.print_python_list.argtypes = [ctypes.py_object]
 lib.print_python_bytes.argtypes = [ctypes.py_object]
 s = b"Hello"
-lib.print_python_bytes(s);
-b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
-lib.print_python_bytes(b);
-b = b'What does the \'b\' character do in front of a string literal?';
-lib.print_python_bytes(b);
+lib.print_python_bytes(s)
+b = b'\xff\xf8\x00\x00\x00\x00\x00\x00'
+lib.print_python_bytes(b)
+b = b'What does the \'b\' character do in front of a string literal?'
+lib.print_python_bytes(b)
 l = [b'Hello', b'World']
 lib.print_python_list(l)
 del l[1]
@@ -28,4 +28,4 @@ l.pop()
 lib.print_python_list(l)
 l = ["Holberton"]
 lib.print_python_list(l)
-lib.print_python_bytes(l);
+lib.print_python_bytes(l)
--- 0x08_CPython/tests/3-tests.py
+++ 0x08_CPython/tests/3-tests.py
@@ -7,11 +7,11 @@ lib.print_python_list.argtypes = [ctypes.py_object]
 lib.print_python_bytes.argtypes = [ctypes.py_object]
 lib.print_python_float.argtypes = [ctypes.py_object]
 s = b"Hello"
-lib.print_python_bytes(s);
-b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
-lib.print_python_bytes(b);
-b = b'What does the \'b\' character do in front of a string literal?';
-lib.print_python_bytes(b);
+lib.print_python_bytes(s)
+b = b'\xff\xf8\x00\x00\x00\x00\x00\x00'
+lib.print_python_bytes(b)
+b = b'What does the \'b\' character do in front of a string literal?'
+lib.print_python_bytes(b)
 l = [b'Hello', b'World']
 lib.print_python_list(l)
 del l[1]
@@ -31,11 +31,11 @@ l.pop()
 lib.print_python_list(l)
 l = ["Holberton"]
 lib.print_python_list(l)
-lib.print_python_bytes(l);
+lib.print_python_bytes(l)
 f = 3.14
-lib.print_python_float(f);
+lib.print_python_float(f)
 l = [-1.0, -0.1, 0.0, 1.0, 3.14, 3.14159, 3.14159265, 3.141592653589793238462643383279502884197169399375105820974944592307816406286]
 print(l)
-lib.print_python_list(l);
-lib.print_python_float(l);
-lib.print_python_list(f);
+lib.print_python_list(l)
+lib.print_python_float(l)
+lib.print_python_list(f)
